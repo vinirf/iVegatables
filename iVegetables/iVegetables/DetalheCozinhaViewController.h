46634ba40086938202322ca678a166c692bfd673
Emerson Barros <emerson.sbarros@gmail.com>
2014-04-23 17:25:02 -0300

    last

---
+++ iVegetables/iVegetables/DetalheCozinhaViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetalheCozinhaViewController.h
+//  iVegetables
+//
+//  Created by EMERSON DE SOUZA BARROS on 23/04/14.
+//  Copyright (c) 2014 Vinicius Resende Fialho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetalheCozinhaViewController : UIViewController <UIWebViewDelegate>
+
+@end
